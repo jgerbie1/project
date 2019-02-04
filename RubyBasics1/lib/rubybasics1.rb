556b8a3b629184ea720f8745ae172b0f5987b8e8
Judy Gerbier <jgerbie1@uncc.com>
2019-02-04 00:51:16 +0000

    RubyBasics1 - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -7,10 +7,37 @@ end
 
 # Part II
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+      return 0;
+  else if arr.length == 1
+      return arr.at(0)
+  else
+n = arr.max
+ arr.delete_at(arr.index(n))
+ m = arr.max
+total = m + n
+end
+return total
 end
 
 # Part III
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.empty?
+    retrun false
+  else if arr.length == 1
+    retun false
+   else
+     arr.each do |i|
+       arr.each do |j|
+       if arr[i] + arr[j] == n
+        return true
+       else
+      return false
+       end
+     end
+    end 
+  end
+  end
+end 
 end
