90b932759671755bd3d3476d2679f6c0852141b3
Judy Gerbier <jgerbie1@uncc.com>
2019-02-11 00:28:44 +0000

    Initial Ruby Hashes Commit

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -16,9 +16,20 @@ end
 # Part II
 def array2d_2_hash contact_info, contacts
     # YOUR CODE HERE
+    if contacts_info.empty?
+        return contacts;
+    end 
+    l = 0;
+    feilds = [:email, :phone ]
+    contacts.each do |key,v|
+        feilds.each do |feilds|
+            key[feilds] = contact_info[v].shift 
+        end 
+    end 
+        return contacts 
 end
 
 # Part III
 def hash_2_array contacts
     # YOUR CODE HERE
-end
+end 
