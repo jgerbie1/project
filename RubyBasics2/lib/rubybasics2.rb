39f4f70a38f1db4f4fa307a4e2007a97b04b8b0e
Judy Gerbier <jgerbie1@uncc.com>
2019-02-13 21:37:11 +0000

    Ruby Basics 2 - Part 3

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -10,19 +10,25 @@ end
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
- a = s.chr
- a.downcase
- b = a[/(?<vowel>[aeiou])(?<non_vowel>[^aeiou])/, "non_vowel"]
-  return a == b
-end
+# a = s.chr
+# a.downcase.gsub(/\w/)
+# # return a == b a[/(?<vowel>[aeiou])(?<non_vowel>[^aeiou])/, "non_vowel"]
+#   return s.downcase.scan(/\A/) == a
+  if s.chr == s.scan(/[a-zA-Z]/)[0]
+   return s.chr.downcase == s.chr.downcase.scan(/[^aeiou]/)[0]
+ end 
+ end 
 
 # Part III
 def binary_multiple_of_4? s
   # YOUR CODE HERE
-  a = s.bytesize 
-  if (a%4 == 0)
-    return true;
-  else 
-    return false;
-  end 
+  #cpattern of characters 
+  if  s.match?(/\D/) == false
+    if s.match?(/[2-9]/) == false
+      if s.scan(/\d/)[-1] == "0"
+      s.chop
+      return s.scan(/\d/)[-1] == "0"
+       end 
+     end
+   end
 end
