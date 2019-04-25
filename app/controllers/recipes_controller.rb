4718c9167b59f8ade0d6c9384f810467d61b64fd
Steven Falkl <sfalkl@uncc.edu>
2019-04-25 23:05:52 +0000

    Back Button functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,17 +1,35 @@
 class RecipesController < ApplicationController
+   
     def index
         @recipes = Recipe.all
     end
     def new
+        @recipe = Recipe.new
     end
     def show
         @recipe = Recipe.find(params[:id])
     end
     def create 
-        @recipe = Recipe.new(recipes_params)
-        @recipe.save
-        redirect_to @recipe
+        @recipe = Recipe.new(recipe_params)
+        if   @recipe.save
+            redirect_to @recipe
+        else 
+            render 'new'
+        end
     end
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update(recipe_params)
+            redirect_to @recipe
+        else
+            render 'edit'
+        end
+    end
+
     private
         def recipes_params
             params.require(:recipe).permit(:title, :instruction, :ingredients, :description)
