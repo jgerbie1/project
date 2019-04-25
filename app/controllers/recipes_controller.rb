64311452e3056e837e67affc7371cc035b66d8aa
omenafee <omenafee@uncc.edu>
2019-04-25 14:02:04 +0000

    List all recipes in index.html.erb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,19 @@
 class RecipesController < ApplicationController
+    def index
+        @recipes = Recipe.all
+    end
     def new
     end
     def show
         @recipe = Recipe.find(params[:id])
     end
     def create 
-        @recipe = Recipe.new(params[:recipes])
+        @recipe = Recipe.new(recipes_params)
         @recipe.save
         redirect_to @recipe
     end
     private
         def recipes_params
-            params.require(:Recipe).permit(:title, :instruction, :ingredients, :description)
+            params.require(:recipe).permit(:title, :instruction, :ingredients, :description)
         end
 end
