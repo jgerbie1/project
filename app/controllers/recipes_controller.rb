faf94d74ff0051039f3ca2546866699354248d38
Steven Falkl <sfalkl@uncc.edu>
2019-04-25 23:24:00 +0000

    Save Recipe Progress

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,20 +3,25 @@ class RecipesController < ApplicationController
     def index
         @recipes = Recipe.all
     end
+    
     def new
         @recipe = Recipe.new
     end
+    
     def show
         @recipe = Recipe.find(params[:id])
     end
+    
     def create 
         @recipe = Recipe.new(recipe_params)
+        
         if   @recipe.save
             redirect_to @recipe
         else 
             render 'new'
         end
     end
+    
     def edit
         @recipe = Recipe.find(params[:id])
     end
